61917ecbc815cbc8761c34f0decef658120d2867
xmilak <126503532+xmilak@users.noreply.github.com>
2023-05-11 11:47:35 -0400

    done

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    public class Car
+    {
+        public Car()
+        {
+            
+        }
+        public Car(string year, string make, string model, string engineNoise, string honkNoise, bool isDriveable)
+        {
+            
+        }
+
+        public string Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDriveable { get; set; }
+
+        public void MakeEngineNoise(string engineNoise)
+        {
+            Console.WriteLine($"The car engine goes {engineNoise}");
+        }
+
+        public void MakeHonkNoise(string honkNoise)
+        {
+            Console.WriteLine($"The car's horn goes {honkNoise}");
+        }
+    }
+}
---
+++ CarLotSimulatorApp/CarLotSimulator/CarLot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    public class CarLot
+    {
+        public CarLot()
+        {
+            
+        }
+        public List<Car> ParkingLot { get; set; } = new List<Car>();
+    }
+}
